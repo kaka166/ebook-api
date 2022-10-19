bc9c858bf5219eef316f5c52192b8f1e3e54c5c2
kaka166 <hafizhfaruq858@gmail.com>
2022-10-19 14:23:49 +0700

    endpoint siswa

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Siswa;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $table = Siswa::all();
+        return response()->json([
+            "message" => "Load data success",
+            "data" => $table
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /*
+        $table = Siswa::create([
+        "nama" => $request->nama,
+        "gender" => $request->gender,
+        "umur" => $request->umur
+        ]);
+
+        return $table;
+        */
+
+        $table = new Siswa();
+        $table->nama = $request->nama;
+        $table->gender = $request->gender;
+        $table->umur = $request->umur;
+        $table->save();
+
+        return response()->json([
+            "message" => "Load data success",
+            "data" => $table
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $table = Siswa::find($id);
+        if($table){
+            return $table;
+        }else {
+            return ["message" => "Data not found"];
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $table = Siswa::find($id);
+        if($table){
+            $table->nama = $request->nama ? $request->nama : $table->nama;
+            $table->gender = $request->gender ? $request->gender : $table->gender;
+            $table->umur = $request->umur ? $request->umur : $table->umur;
+            $table->save();
+
+            return $table;
+        }else{
+            return["message" => "Data not found"];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $table = Siswa::find($id);
+        if($table){
+            $table->delete();
+            return ["message" => "Delete success"];
+        } else{
+            return ["message" => "Data not found"];
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+Route::get('halo', function(){
+    return["me" => "ganss"];
+});
+
+Route::resource('siswa', SiswaController::class);
